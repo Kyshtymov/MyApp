92a528345ebc77c34acb640e4d0c427ce3a96f69
Kyshtymov <dakyshtymov@gmail.com>
2022-05-28 19:06:01 +0700

    Четвёртый коммит, добавлена сумма по критериям, юзер интерфейяс, подсчёт статистики

--- DataAccessLibrary/Class1.cs
+++ DataAccessLibrary/Class1.cs
@@ -22,22 +22,16 @@ namespace DataAccessLibrary
                 insertCommand.Connection = db;
 
 
-                insertCommand.CommandText = " DROP TABLE MyTable;";
-                insertCommand.ExecuteReader();
-
-               // String tableCommand = "CREATE TABLE IF NOT " +
-                 //   "EXISTS MyTable (Primary_Key INTEGER PRIMARY KEY, " +
-                 //   "Text_Entry NVARCHAR(2048) NULL, Income INT)";
                 String tableCommand = "CREATE TABLE IF NOT " +
                     "EXISTS MyTable (id INTEGER PRIMARY KEY, " +
-                    "Amount REAL NULL, Category NVARCHAR(2048))";
+                    "Day INT, Month INT, Year INT, Amount REAL, Category NVARCHAR(2048), Commentary NVARCHAR(2048))";
                 SqliteCommand createTable = new SqliteCommand(tableCommand, db);
 
                 createTable.ExecuteReader();
             }
         }
 
-        public static void AddData(string Amount, string Category, int income)
+        public static void AddData(string Day, string Month, string Year, string Amount, string Category, string Commentary, int income)
         {
 
             string dbpath = System.IO.Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "sqliteSample.db");
@@ -50,17 +44,50 @@ namespace DataAccessLibrary
                 insertCommand.Connection = db;
 
                 // Use parameterized query to prevent SQL injection attacks
-                if (income == 1)
-                {
-                    insertCommand.CommandText = "INSERT INTO MyTable VALUES (NULL, @Entry, @Category);";
-                    insertCommand.Parameters.AddWithValue("@Entry", Amount);
-                }
-                else
-                {
-                    insertCommand.CommandText = "INSERT INTO MyTable VALUES (NULL, @Entry, 0);";
-                    insertCommand.Parameters.AddWithValue("@Entry", "-" + Amount);
-                }
+
+                insertCommand.CommandText = "INSERT INTO MyTable VALUES (NULL, @Day, @Month, @Year, @Entry, @Category, @Commentary);";
+                
+                if (income == 1) insertCommand.Parameters.AddWithValue("@Entry", Amount);
+                else insertCommand.Parameters.AddWithValue("@Entry", "-" + Amount);
+
+                insertCommand.Parameters.AddWithValue("@Day", Day);
+                insertCommand.Parameters.AddWithValue("@Month", Month);
+                insertCommand.Parameters.AddWithValue("@Year", Year);
                 insertCommand.Parameters.AddWithValue("@Category", Category);
+                insertCommand.Parameters.AddWithValue("@Commentary", Commentary);
+
+                insertCommand.ExecuteReader();
+
+                db.Close();
+            }
+
+        }
+
+
+        public static void UpdateData(string ID, string Day, string Month, string Year, string Amount, string Category, string Commentary, int income)
+        {
+
+            string dbpath = System.IO.Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "sqliteSample.db");
+            using (SqliteConnection db =
+              new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+
+                SqliteCommand insertCommand = new SqliteCommand();
+                insertCommand.Connection = db;
+
+                // Use parameterized query to prevent SQL injection attacks
+                
+                insertCommand.CommandText = "UPDATE MyTable SET id = @ID,  Day = @Day, Month = @Month, Year = @Year, Amount = @Entry, Category = @Category, Commentary = @Commentary WHERE id = @ID;";
+                if (income == 1)  insertCommand.Parameters.AddWithValue("@Entry", Amount);
+                else  insertCommand.Parameters.AddWithValue("@Entry", "-" + Amount);
+
+                insertCommand.Parameters.AddWithValue("@ID", ID);
+                insertCommand.Parameters.AddWithValue("@Day", Day);
+                insertCommand.Parameters.AddWithValue("@Month", Month);
+                insertCommand.Parameters.AddWithValue("@Year", Year);
+                insertCommand.Parameters.AddWithValue("@Category", Category);
+                insertCommand.Parameters.AddWithValue("@Commentary", Commentary);
 
                 insertCommand.ExecuteReader();
 
@@ -123,11 +150,19 @@ namespace DataAccessLibrary
 
                 SqliteDataReader query = selectCommand.ExecuteReader();
 
+
                 while (query.Read())
                 {
-                    String Output = query.GetString(0) + "  " +  query.GetString(1) + "  " + query.GetString(2);
+                    string Day = query.GetString(1);
+                    if (Day.Length == 1) Day = "0" + Day;
+
+                    string Month = query.GetString(2);
+                    if (Month.Length == 1) Month = "0" + Month;
+
+                    string Year = query.GetString(3);
+                    if (Year.Length == 1) Year = "0" + Year;
+                    String Output = "ID: " + query.GetString(0) + "  Дата: " + Day + "." + Month + "." + Year + "  Сумма: " + query.GetString(4) + "  Категория: " + query.GetString(5) + "  Комментарий: " + query.GetString(6);
                     entries.Add(Output);
-                    //entries.Add(query.GetString(0));
                 }
 
                 db.Close();
@@ -137,24 +172,51 @@ namespace DataAccessLibrary
         }
 
 
-        public static string Sum()
+        public static string Sum(string ID, string Day, string Month, string Year, string Amount, string Category, string Commentary, bool total, out int Operations)
         {
             float sum = 0;
-
+            Operations = 0;
             string dbpath = System.IO.Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "sqliteSample.db");
             using (SqliteConnection db =
                new SqliteConnection($"Filename={dbpath}"))
             {
                 db.Open();
+                SqliteCommand selectCommand;
+                if ((ID + Day + Month + Year + Amount + Category + Commentary == "") && (!total)) return "0";
 
-                SqliteCommand selectCommand = new SqliteCommand
-                    ("SELECT * from MyTable", db);
+                string IDCom = "", DayCom = "", MonthCom = "", YearCom = "", AmountCom = "", CategoryCom = "", CommentaryCom = "";
+
+                if (ID != "")
+                    IDCom = " id = @ID ";
+                else
+                    IDCom = " id > 0 ";
+
+                if (Day != "") DayCom = " AND Day = @Day ";
+                if (Month != "") MonthCom = " AND Month = @Month ";
+                if (Year != "") YearCom = " AND Year = @Year ";
+                if (Amount != "") AmountCom = " AND Amount = @Amount ";
+                if (Category != "") CategoryCom = " AND Category = @Category ";
+                if (Commentary != "") CommentaryCom = " AND Commentary = @Commentary ";
+                if (total) selectCommand = new SqliteCommand("SELECT Amount from MyTable", db);
+                else
+                {
+                    string Command = "SELECT Amount from MyTable Where " + IDCom + DayCom + MonthCom + YearCom + AmountCom + CategoryCom + CommentaryCom;
+                    selectCommand = new SqliteCommand(Command, db);
+                    if (ID != "") selectCommand.Parameters.AddWithValue("@ID", ID);
+                    if (Day != "") selectCommand.Parameters.AddWithValue("@Day", Day);
+                    if (Month != "") selectCommand.Parameters.AddWithValue("@Month", Month);
+                    if (Year != "") selectCommand.Parameters.AddWithValue("@Year", Year);
+                    if (Amount != "") selectCommand.Parameters.AddWithValue("@Amount", Amount);
+                    if (Category != "") selectCommand.Parameters.AddWithValue("@Category", Category);
+                    if (Commentary != "") selectCommand.Parameters.AddWithValue("@Commentary", Commentary);
+                }
 
                 SqliteDataReader query = selectCommand.ExecuteReader();
 
                 while (query.Read())
                 {
-                      sum += query.GetFloat(1);
+                    Operations++;
+                    sum += query.GetFloat(0);
                 }
 
                 db.Close();
--- MyApp/MainPage.xaml.cs
+++ MyApp/MainPage.xaml.cs
@@ -23,42 +23,120 @@ namespace MyApp
     /// </summary>
     public sealed partial class MainPage : Page
     {
+
+        bool History = false;
+        bool Update = false;
+        bool ShowSum = false;
         public MainPage()
         {
             this.InitializeComponent();
-            Sum_Box.Text = "Total = 0" + DataAccess.Sum();
         }
 
-        private async void Income_Click(object sender, RoutedEventArgs e)
+        private void Log(string LogText)
         {
-            MediaElement mediaElement = new MediaElement();
-            var synth = new Windows.Media.SpeechSynthesis.SpeechSynthesizer();
-            Windows.Media.SpeechSynthesis.SpeechSynthesisStream stream = await synth.SynthesizeTextToStreamAsync("Hello, World!");
-            mediaElement.SetSource(stream, stream.ContentType);
-            mediaElement.Play();
+            int pos = 0;
+            List<String> entries = new List<string>();
+            if (History) entries = DataAccess.GetData();
+         
+            if (ShowSum)
+            {
+                int Operations, OperationsTotal;
+                string sum1 = DataAccess.Sum(Delete_ID.Text, Input_Day.Text, Input_Month.Text, Input_Year.Text, Input_Amount.Text, Input_Category.Text, Input_Commentary.Text, false, out Operations);
+                string sum2 = DataAccess.Sum(Delete_ID.Text, Input_Day.Text, Input_Month.Text, Input_Year.Text, Input_Amount.Text, Input_Category.Text, Input_Commentary.Text, true, out OperationsTotal);
+                string CommandSum = "Прибыль по критериям = " + sum1 + "  Количество операций по критериям = " + Operations;
+                string CommandSum2 = "Полная прибыль = " + sum2 + "  Полное количество операций = " + OperationsTotal;
+
+                entries.Insert(0, CommandSum);
+                entries.Insert(1, CommandSum2);
+                pos += 2;
+            }
+            if (LogText != "")
+            {
+                entries.Insert(pos, LogText);
+                pos++;
+            }
+            if (History) entries.Insert(pos, "История операций:");
+            Output.ItemsSource = entries;
         }
+
+
+        private void WriteInTable(int income)
+        {
+            string Category = Input_Category.Text;
+            if (Input_Category.Text == "")  Category = "Прочее";
+
+            int Day = 0;
+            bool CheckDay = int.TryParse(Input_Day.Text, out Day);
+            if ((CheckDay) && (Day > 0) && (Day < 32)) CheckDay = true;
+            else
+            {
+                CheckDay = false;
+                Log("Введите день от 1 до 31 числа");
+            }
+
+
+            int Month = 0;
+            bool CheckMonth = int.TryParse(Input_Month.Text, out Month);
+            if ((CheckMonth) && (Month > 0) && (Month < 13)) CheckMonth = true;
+            else
+            {
+                CheckMonth = false;
+                Log("Введите месяц от 1 до 12");
+
+            }
+
+            int Year = 0;
+            bool CheckYear = int.TryParse(Input_Year.Text, out Year);
+            if ((CheckYear) && (Year > 0) && (Year < 32)) CheckYear = true;
+            else
+            {
+                CheckYear = false;
+                Log("Введите год от 0");
+            }
+
+
+            float Amount = 0;
+            bool CheckAmount = float.TryParse(Input_Amount.Text, out Amount);
+            if ((CheckAmount) && (Amount > 0) && (Year < 32)) CheckAmount = true;
+            else
+            {
+                CheckYear = false;
+                Log("Введите сумму от 0");
+            }
+
+            bool Check = CheckDay && CheckMonth && CheckYear && CheckAmount;
+            if ((Check) && (!Update))
+            {
+                DataAccess.AddData(Input_Day.Text, Input_Month.Text, Input_Year.Text, Input_Amount.Text, Category, Input_Commentary.Text, income);
+                Log("");
+            }
+        
+            if ((Check) && (Update))
+            {
+                if (Delete_ID.Text != "")
+                {
+                    int ID = 0;
+                    bool CheckID = int.TryParse(Delete_ID.Text, out ID);
+                    if ((CheckID) && (ID > 0))
+                    {
+                        DataAccess.UpdateData(Delete_ID.Text, Input_Day.Text, Input_Month.Text, Input_Year.Text, Input_Amount.Text, Category, Input_Commentary.Text, income);
+                        Log("");
+                    }
+                }
+                else Log("Введите ID для обновления");
+            }
+        }
+
         private void IncomeData(object sender, RoutedEventArgs e)
         {
-            DataAccess.AddData(Input_Amount.Text, Input_Category.Text, 1);
-
-            if (Output.Visibility == Visibility.Visible) Output.ItemsSource = DataAccess.GetData();
-            Sum_Box.Text = "Total = " + DataAccess.Sum();
+            WriteInTable(1);
         }
 
 
         private void OutcomeData(object sender, RoutedEventArgs e)
         {
-            DataAccess.AddData(Input_Amount.Text, Input_Category.Text, 0);
 
-            List<String> entries = new List<string>();
-            if (Output.Visibility == Visibility.Visible)
-            {
-                entries = DataAccess.GetData();
-
-                entries.Insert(0, "Total = " + DataAccess.Sum());
-                Output.ItemsSource = entries;
-            }
-            Sum_Box.Text = "Total = " + DataAccess.Sum();
+            WriteInTable(0);
         }
 
         private void DeleteData(object sender, RoutedEventArgs e)
@@ -66,18 +144,14 @@ namespace MyApp
             if (Delete_ID.Text != "")
             {
                 DataAccess.DeleteData(Delete_ID.Text);
-
-                if (Output.Visibility == Visibility.Visible) Output.ItemsSource = DataAccess.GetData();
-                Sum_Box.Text = DataAccess.Sum();
-            }
+                Log("");
+           }
         }
 
         private void DeleteAllData(object sender, RoutedEventArgs e)
         {
             DataAccess.DeleteAllData();
-
-            if (Output.Visibility == Visibility.Visible) Output.ItemsSource = DataAccess.GetData();
-            Sum_Box.Text = "Total = 0";
+            Log("");
         }
         private void ToggleSwitch_Toggled(object sender, RoutedEventArgs e)
         {
@@ -86,14 +160,54 @@ namespace MyApp
             {
                 if (toggleSwitch.IsOn == true)
                 {
-                    Output.Visibility = Visibility.Visible; 
-                    Output.ItemsSource = DataAccess.GetData();
+                    History = true;
+                    Log("");
                 }
                 else
                 {
-                    Output.Visibility = Visibility.Collapsed;
+                    History = false;
+                    Log("");
                 }
             }
+        }
+
+        private void ToggleSwitch_Toggled_Update(object sender, RoutedEventArgs e)
+        {
+            ToggleSwitch toggleSwitch = sender as ToggleSwitch;
+            if (toggleSwitch != null)
+            {
+                if (toggleSwitch.IsOn == true)
+                { 
+                    Update = true;
+                }
+                else
+                {
+                    Update = false;
+                }
+            }
+        }
+
+        private void ToggleSwitch_Toggled_Sum(object sender, RoutedEventArgs e)
+        {
+            ToggleSwitch toggleSwitch = sender as ToggleSwitch;
+            if (toggleSwitch != null)
+            {
+                if (toggleSwitch.IsOn == true)
+                {
+                    ShowSum = true;
+                    Log("");
+                }
+                else
+                {
+                    ShowSum = false;
+                }
+            }
+
+        }
+
+        private void UpdateSum(object sender, RoutedEventArgs e)
+        {
+            Log("");
         }
     }
 }
