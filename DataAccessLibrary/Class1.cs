b74e371ff5e5df027b5dd6e93ec4b24c2c8cd0e8
Kyshtymov <dakyshtymov@gmail.com>
2022-05-28 12:35:02 +0700

    First Commit

---
+++ DataAccessLibrary/Class1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+
+namespace DataAccessLibrary
+{
+    public static class DataAccess
+    {
+        public async static void InitializeDatabase()
+        {
+            await Windows.Storage.ApplicationData.Current.LocalFolder.CreateFileAsync("sqliteSample.db", Windows.Storage.CreationCollisionOption.OpenIfExists);
+            string dbpath = System.IO.Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "sqliteSample.db");
+            using (SqliteConnection db =
+               new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+
+                SqliteCommand insertCommand = new SqliteCommand();
+                insertCommand.Connection = db;
+
+
+                insertCommand.CommandText = " DROP TABLE MyTable;";
+                insertCommand.ExecuteReader();
+
+               // String tableCommand = "CREATE TABLE IF NOT " +
+                 //   "EXISTS MyTable (Primary_Key INTEGER PRIMARY KEY, " +
+                 //   "Text_Entry NVARCHAR(2048) NULL, Income INT)";
+                String tableCommand = "CREATE TABLE IF NOT " +
+                    "EXISTS MyTable (id INTEGER PRIMARY KEY, " +
+                    "Amount REAL NULL, Category NVARCHAR(2048))";
+                SqliteCommand createTable = new SqliteCommand(tableCommand, db);
+
+                createTable.ExecuteReader();
+            }
+        }
+
+        public static void AddData(string Amount, string Category, int income)
+        {
+
+            string dbpath = System.IO.Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "sqliteSample.db");
+            using (SqliteConnection db =
+              new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+
+                SqliteCommand insertCommand = new SqliteCommand();
+                insertCommand.Connection = db;
+
+                // Use parameterized query to prevent SQL injection attacks
+                if (income == 1)
+                {
+                    insertCommand.CommandText = "INSERT INTO MyTable VALUES (NULL, @Entry, @Category);";
+                    insertCommand.Parameters.AddWithValue("@Entry", Amount);
+                }
+                else
+                {
+                    insertCommand.CommandText = "INSERT INTO MyTable VALUES (NULL, @Entry, 0);";
+                    insertCommand.Parameters.AddWithValue("@Entry", "-" + Amount);
+                }
+                insertCommand.Parameters.AddWithValue("@Category", Category);
+
+                insertCommand.ExecuteReader();
+
+                db.Close();
+            }
+
+        }
+
+        public static void DeleteData(string idText)
+        {
+            string dbpath = System.IO.Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "sqliteSample.db");
+            using (SqliteConnection db =
+              new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+
+                SqliteCommand insertCommand = new SqliteCommand();
+                insertCommand.Connection = db;
+
+                insertCommand.CommandText = "DELETE FROM MyTable WHERE id = @ID;";
+                insertCommand.Parameters.AddWithValue("@ID", idText);
+                insertCommand.ExecuteReader();
+
+                db.Close();
+            }
+
+        }
+
+
+        public static void DeleteAllData()
+        {
+            string dbpath = System.IO.Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "sqliteSample.db");
+            using (SqliteConnection db =
+              new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+
+                SqliteCommand insertCommand = new SqliteCommand();
+                insertCommand.Connection = db;
+
+                insertCommand.CommandText = "DELETE FROM MyTable;";
+                insertCommand.ExecuteReader();
+
+                db.Close();
+            }
+
+        }
+        public static List<String> GetData()
+        {
+            List<String> entries = new List<string>();
+
+            string dbpath = System.IO.Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "sqliteSample.db");
+            using (SqliteConnection db =
+               new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+
+                SqliteCommand selectCommand = new SqliteCommand
+                    ("SELECT * from MyTable", db);
+
+                SqliteDataReader query = selectCommand.ExecuteReader();
+
+                while (query.Read())
+                {
+                    String Output = query.GetString(0) + "  " +  query.GetString(1) + "  " + query.GetString(2);
+                    entries.Add(Output);
+                    //entries.Add(query.GetString(0));
+                }
+
+                db.Close();
+            }
+
+            return entries;
+        }
+
+
+        public static string Sum()
+        {
+            float sum = 0;
+
+            string dbpath = System.IO.Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "sqliteSample.db");
+            using (SqliteConnection db =
+               new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+
+                SqliteCommand selectCommand = new SqliteCommand
+                    ("SELECT * from MyTable", db);
+
+                SqliteDataReader query = selectCommand.ExecuteReader();
+
+                while (query.Read())
+                {
+                      sum += query.GetFloat(1);
+                }
+
+                db.Close();
+            }
+
+            return sum.ToString();
+        }
+    }
+}
---
+++ MyApp/MainPage.xaml.cs
@@ -0,0 +1,98 @@
+﻿using DataAccessLibrary;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
+
+namespace MyApp
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Sum_Box.Text = "Total = 0" + DataAccess.Sum();
+        }
+
+        private async void Income_Click(object sender, RoutedEventArgs e)
+        {
+            MediaElement mediaElement = new MediaElement();
+            var synth = new Windows.Media.SpeechSynthesis.SpeechSynthesizer();
+            Windows.Media.SpeechSynthesis.SpeechSynthesisStream stream = await synth.SynthesizeTextToStreamAsync("Hello, World!");
+            mediaElement.SetSource(stream, stream.ContentType);
+            mediaElement.Play();
+        }
+        private void IncomeData(object sender, RoutedEventArgs e)
+        {
+            DataAccess.AddData(Input_Amount.Text, Input_Category.Text, 1);
+
+            if (Output.Visibility == Visibility.Visible) Output.ItemsSource = DataAccess.GetData();
+            Sum_Box.Text = "Total = " + DataAccess.Sum();
+        }
+
+
+        private void OutcomeData(object sender, RoutedEventArgs e)
+        {
+            DataAccess.AddData(Input_Amount.Text, Input_Category.Text, 0);
+
+            List<String> entries = new List<string>();
+            if (Output.Visibility == Visibility.Visible)
+            {
+                entries = DataAccess.GetData();
+                entries.Insert(0, "Total = " + DataAccess.Sum());
+                Output.ItemsSource = entries;
+            }
+            Sum_Box.Text = "Total = " + DataAccess.Sum();
+        }
+
+        private void DeleteData(object sender, RoutedEventArgs e)
+        {
+            if (Delete_ID.Text != "")
+            {
+                DataAccess.DeleteData(Delete_ID.Text);
+
+                if (Output.Visibility == Visibility.Visible) Output.ItemsSource = DataAccess.GetData();
+                Sum_Box.Text = DataAccess.Sum();
+            }
+        }
+
+        private void DeleteAllData(object sender, RoutedEventArgs e)
+        {
+            DataAccess.DeleteAllData();
+
+            if (Output.Visibility == Visibility.Visible) Output.ItemsSource = DataAccess.GetData();
+            Sum_Box.Text = "Total = 0";
+        }
+        private void ToggleSwitch_Toggled(object sender, RoutedEventArgs e)
+        {
+            ToggleSwitch toggleSwitch = sender as ToggleSwitch;
+            if (toggleSwitch != null)
+            {
+                if (toggleSwitch.IsOn == true)
+                {
+                    Output.Visibility = Visibility.Visible; 
+                    Output.ItemsSource = DataAccess.GetData();
+                }
+                else
+                {
+                    Output.Visibility = Visibility.Collapsed;
+                }
+            }
+        }
+    }
+}
